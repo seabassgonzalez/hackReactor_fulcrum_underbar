9cc7aa9a8bef4302c4151f0aac6db7f4e325b01b
seabassgonzalez <seabassgonzalez@gmail.com>
2016-04-09 16:51:29 -0700

    Reimplement _.extend

--- underbar/src/underbar.js
+++ underbar/src/underbar.js
@@ -304,7 +304,13 @@
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
-  _.extend = function(obj) {
+   _.extend = function(obj) {
+    _.each(arguments, function(argumentElement){
+        _.each(argumentElement, function(argumentElement, key){
+            obj[key] = argumentElement;
+        });
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
